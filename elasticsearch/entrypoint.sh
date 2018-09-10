3c36e3c2b99d026be38720c8266e213b49fc934b
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2018-09-10 20:32:21 +0100

    Create keystore and set bootstrap password

--- elasticsearch/entrypoint.sh
+++ elasticsearch/entrypoint.sh
@@ -8,4 +8,7 @@ printf "%s" "$ES_SSL_CA"     > /usr/share/elasticsearch/config/ca.crt
 printf "%s" "$ES_SSL_KEY"    > /usr/share/elasticsearch/config/node.key
 printf "%s" "$ES_SSL_CERT"   > /usr/share/elasticsearch/config/node.crt
 
+bin/elasticsearch-keystore create
+echo "$ELASTIC_PASSWORD" | bin/elasticsearch-keystore add "bootstrap.password" --stdin
+
 exec /usr/local/bin/docker-entrypoint.sh
