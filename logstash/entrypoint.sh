8bb3cdae54caf93e51c8a3f9721633fcd3f8ea46
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2020-01-15 14:14:00 +0000

    Add the Zscaler NSS syslog pipeline

--- logstash/entrypoint.sh
+++ logstash/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-keytool -genkey -keyalg RSA -alias selfsigned -keystore keystore.jks -validity 360 -keysize 2048 -noprompt -dname "CN=logstash.uktrade.io, OU=ID, O=DIT, L=London, S=London, C=GB" -storepass password -keypass password
+keytool -genkey -keyalg RSA -alias selfsigned -keystore /usr/share/logstash/config/keystore.jks -validity 360 -keysize 2048 -noprompt -dname "CN=logstash.uktrade.io, OU=ID, O=DIT, L=London, S=London, C=GB" -storepass password -keypass password
 
 exec /usr/local/bin/docker-entrypoint
