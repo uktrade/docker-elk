f407c35f055f522a8b49cf4a2ea14c56065b5f0d
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2020-03-10 14:34:09 +0000

    Provide system_key via env var

--- elasticsearch/entrypoint.sh
+++ elasticsearch/entrypoint.sh
@@ -5,11 +5,14 @@
 : "${ES_SSL_CERT:?Set ES_SSL_CERT using --env}"
 : "${SAML_KEY:?Set SAML_KEY using --env}"
 : "${SAML_CERT:?Set SAML_CERT using --env}"
+: "${SYSTEM_KEY}:?Set SYSTEM_KEY using --env}"
 
 echo -e "$ES_SSL_CA"     > /usr/share/elasticsearch/config/ca.crt
 echo -e "$ES_SSL_KEY"    > /usr/share/elasticsearch/config/node.key
 echo -e "$ES_SSL_CERT"   > /usr/share/elasticsearch/config/node.crt
 echo -e "$SAML_KEY"      > /usr/share/elasticsearch/config/saml/saml.key
 echo -e "$SAML_CERT"     > /usr/share/elasticsearch/config/saml/saml.crt
+echo "$SYSTEM_KEY" | base64 -d > /usr/share/elasticsearch/config/system_key
+chmod 600 /usr/share/elasticsearch/config/system_key
 
 exec /usr/local/bin/docker-entrypoint.sh
