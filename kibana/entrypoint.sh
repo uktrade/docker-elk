918f685ce850def107052df6fe1a51944e35da49
Lyndon Garvey <lyndon.garvey@digital.trade.gov.uk>
2020-04-09 22:47:02 +0100

    add kibana entrypoint.sh

---
+++ kibana/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+: "${SSL_CERTIFICATE:?Set SSL_CERTIFICATE using --env}"
+: "${SSL_KEY:?Set SSL_KEY using --env}"
+
+echo -e "$SSL_CERTIFICATE"     > /usr/share/kibana/config/node.crt
+echo -e "$SSL_KEY"    > /usr/share/kibana/config/node.key
+
+echo "---------------------------------"
+cat /usr/share/kibana/config/node.key
+echo "---------------------------------"
+cat /usr/share/kibana/config/node.crt
+echo "---------------------------------"
+
+exec /usr/local/bin/kibana-docker
