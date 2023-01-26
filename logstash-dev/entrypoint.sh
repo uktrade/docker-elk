a27a7f0476f488e5640c26329781fe9323bb3b50
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2023-01-26 16:20:13 +0000

    Replace keytool with logstash-keystore

--- logstash-dev/entrypoint.sh
+++ logstash-dev/entrypoint.sh
@@ -7,6 +7,10 @@ set -euo pipefail
 : "${PASSWORD:?Set PASSWORD using --env}"
 : "${PASSWORD:?Set PASSWORD using --env}"
 
-keytool -genkey -keyalg RSA -alias selfsigned -keystore /usr/share/logstash/config/keystore.jks -validity 360 -keysize 2048 -noprompt -dname "CN=logstash.uktrade.io, OU=ID, O=DIT, L=London, S=London, C=GB" -storepass password -keypass password
+# keytool -genkey -keyalg RSA -alias selfsigned -keystore /usr/share/logstash/config/keystore.jks -validity 360 -keysize 2048 -noprompt -dname "CN=logstash.uktrade.io, OU=ID, O=DIT, L=London, S=London, C=GB" -storepass password -keypass password
+set +o history
+export LOGSTASH_KEYSTORE_PASS=${PASSWORD}
+set -o history
+bin/logstash-keystore create
 
 exec /usr/local/bin/docker-entrypoint
