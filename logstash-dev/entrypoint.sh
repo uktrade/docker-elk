f424b681acd9df687ff79636ade66b1448e6cc8b
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2023-01-26 15:18:01 +0000

    Testing Logstash 7.17.8 in dev

--- logstash-dev/entrypoint.sh
+++ logstash-dev/entrypoint.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-set +o history
-export LOGSTASH_KEYSTORE_PASS=mypassword
-set -o history
-bin/logstash-keystore create
+set -euo pipefail
+
+# Validate environment variables
+: "${USERNAME:?Set USERNAME using --env}"
+: "${PASSWORD:?Set PASSWORD using --env}"
+: "${PASSWORD:?Set PASSWORD using --env}"
+
+keytool -genkey -keyalg RSA -alias selfsigned -keystore /usr/share/logstash/config/keystore.jks -validity 360 -keysize 2048 -noprompt -dname "CN=logstash.uktrade.io, OU=ID, O=DIT, L=London, S=London, C=GB" -storepass password -keypass password
 
 exec /usr/local/bin/docker-entrypoint
