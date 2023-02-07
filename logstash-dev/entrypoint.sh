c77f9dd0b66788c16f30a2f23aa1781d6f7cdea9
gareth-e <53176667+ch-gareth@users.noreply.github.com>
2023-02-07 16:56:37 +0000

    Revert logstash-dev changes.

--- logstash-dev/entrypoint.sh
+++ logstash-dev/entrypoint.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 
-set -euo pipefail
-
-# Validate environment variables
-: "${USERNAME:?Set USERNAME using --env}"
-: "${PASSWORD:?Set PASSWORD using --env}"
-: "${PASSWORD:?Set PASSWORD using --env}"
-
-# keytool -genkey -keyalg RSA -alias selfsigned -keystore /usr/share/logstash/config/keystore.jks -validity 360 -keysize 2048 -noprompt -dname "CN=logstash.uktrade.io, OU=ID, O=DIT, L=London, S=London, C=GB" -storepass password -keypass password
 set +o history
-export LOGSTASH_KEYSTORE_PASS=password
+export LOGSTASH_KEYSTORE_PASS=mypassword
 set -o history
-bin/logstash-keystore --path.settings /usr/share/logstash/config create
+bin/logstash-keystore create
 
 exec /usr/local/bin/docker-entrypoint
